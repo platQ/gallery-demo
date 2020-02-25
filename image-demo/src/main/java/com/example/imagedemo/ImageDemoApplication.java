3d50e6b109a2dc366cc4f4d1b93b1e2b30e8e588
Joshua <joshua.platkiw@gmail.com>
2020-02-25 15:17:29 +0000

    Initial spring set up and file structure

---
+++ image-demo/src/main/java/com/example/imagedemo/ImageDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.imagedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageDemoApplication.class, args);
+	}
+
+}
